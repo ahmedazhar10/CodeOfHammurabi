b20be25a0a0267e059c16491c8909c97f39589de
Ahmed Azhar <ahmedazhar@Ahmeds-MacBook-Pro.local>
2020-10-10 19:29:42 +0500

    Completed People category

--- console.py
+++ console.py
@@ -31,16 +31,14 @@ while(True):
         print("Invalid Category")
         continue
     if prompt_category == 1:
-        print("1) Is it about a scandal?") #DONE   
+        print("1) Is it about a scandal?")  
         print("2) Is it about an accusation?")
         print("3) Is it about satisfying the elders to impose a fine?")
-        #    -> Yes -> penalty -> cite law
-        #    -> No  -> Can't pass judgement -> Redo Program  
         print("4) Is it about an error in judgement?")
         #    -> Yes -> Penalty -> Redo Program
 
         prompt_case = int(input("Enter number associated with the question: "))
-        if prompt_case != 1 and prompt_case != 2:
+        if prompt_case != 1 and prompt_case != 2 and prompt_case != 3 and prompt_case != 4:
             print("Invalid response!")
             continue
         if prompt_case == 1:
@@ -52,14 +50,14 @@ while(True):
                 continue
             if prompt_char == 'y' or prompt_char == 'Y':
                 print("Judgment shan't be passed")
-                print("Adjourned!")
+                print("Court adjourned!")
                 continue
             if prompt_char == 'n' or prompt_char == 'N':
                 print("Judgment has passed")
                 print("Put the Accuser to DEATH!")
                 print(law_dict['People'][0]) #Law citation
         if prompt_case == 2:
-            print("Is it a False accusation") #-> yes -> penalty -> cite law
+            print("Is it a False accusation")
             prompt_char = input("Enter character: Yes[y] or No[n]")
             #Response Check
             if prompt_char != "y" and prompt_char != "Y" and prompt_char != "N" and prompt_char != "n":
@@ -84,3 +82,36 @@ while(True):
                     print("Judgment has passed")
                     print("Accuser shall take possession of the house of the accused")
                     print(law_dict['People'][1])
+        if prompt_case == 3:
+            print("Did they satisfy the elders to impose a fine?")
+            prompt_char = input("Enter character: Yes[y] or No[n]")
+            #Response Check
+            if prompt_char != "y" and prompt_char != "Y" and prompt_char != "N" and prompt_char != "n":
+                print("Error: Invalid Response") 
+                continue
+            if prompt_char == 'y' or prompt_char == 'Y':
+                print("Judgment has passed")
+                print("Guilty shall receive a fine")
+                print(law_dict['People'][3]) #Law citation
+            if prompt_char == "n" or prompt_char == "N":
+                print("Judgment shan't be passed")
+                print("Court adjourned!")
+                continue
+        if prompt_case == 4:
+            print("Did the judge err in their decision?")
+            prompt_char = input("Enter character: Yes[y] or No[n]")
+            #Response Check
+            if prompt_char != "y" and prompt_char != "Y" and prompt_char != "N" and prompt_char != "n":
+                print("Error: Invalid Response") 
+                continue
+            if prompt_char == "n" or prompt_char == "N":
+                print("Judgment shan't be passed")
+                print("Court adjourned!")
+                continue
+            if prompt_char == 'y' or prompt_char == 'Y':
+                print("Judgment has passed")
+                print("Judge shall pay 12 times the fine set by them \n Judge shall be removed from the judge's bench permanently")
+                print(law_dict['People'][4]) #Law citation
+
+
+      
